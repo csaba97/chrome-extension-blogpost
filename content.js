6fbadcf5010dee3de4eb51dc46c5c618d5e3f282
csaba97 <csabagabor97@google.com>
2018-08-22 20:14:50 +0300

    replaced images

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+  images[i].src = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_960_720.jpg' + images[i].width + '/' + images[i].height;
 }
